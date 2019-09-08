dbecc7063457d78c595c123729d5801191b3fdf4
Josue Rodriguez Garcia <Josue.Rodriguez@fundacion-jala.org>
2019-09-08 05:13:52 -0400

    add . in coment

--- src/test/java/runner/RunCukesTest.java
+++ src/test/java/runner/RunCukesTest.java
@@ -1,7 +1,7 @@
 package runner;
 
 /**
- * This class is used to run the framework by console
+ * This class is used to run the framework by console.
  *
  * @author Josue Rodriguez Garcia.
  * @version 0.0.1
